1dcb77b8beaa7475e5e1d64e052e884858a9f07b
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2019-02-14 23:17:14 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector("div.target");
 }
+
+function deepestChild() {
+  return document.querySelector("#grand-node div div div div");
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll(".ranked-list");
+
+  for (let i = 0; i < rankedLists.length; i++) {
+    const children = rankedLists[i].children;
+
+    for (let i = 0; i < children.length; i++) {
+      children[i].innerHTML = parseInt(children[i].innerHTML) + n;
+    }
+  }
+}
